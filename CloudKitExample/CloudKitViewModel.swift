3ed3585343d631bca9934136899363db3a338b98
MAHESHWARAN <maheshwaran@eam360.com>
2023-11-19 09:10:00 +0530

    ContentView  - Handled crud operation for cloudKit

--- CloudKitExample/CloudKitViewModel.swift
+++ CloudKitExample/CloudKitViewModel.swift
@@ -10,6 +10,11 @@ import CloudKit
 
 class CloudKitViewModel: ObservableObject {
   
+  @Published var textValue = ""
+  @Published var records = [Item]()
+  
+  // MARK: - Status
+  
   @Published var userStatus = false
   @Published var isSignedIn = false
   @Published var error = ""
@@ -19,6 +24,8 @@ class CloudKitViewModel: ObservableObject {
     getiCloudStatus()
     requestPermission()
     getUserRecordID()
+    
+    getItems()
   }
   
   func getiCloudStatus() {
@@ -79,5 +86,122 @@ extension CloudKitViewModel {
   
   enum CloudKitError: String, LocalizedError {
     case notFound, notDetermine, restricted, unknown
+  }
+}
+
+// MARK: - CRUD
+
+extension CloudKitViewModel {
+  
+  func addButtonClicked() {
+    guard !textValue.isEmpty else { return }
+    addItem(textValue)
+  }
+  
+  func addItem(_ name: String) {
+    let newRecord = CKRecord(recordType: "Records")
+    newRecord["name"] = name
+    
+    saveItem(newRecord)
+  }
+  
+  // MARK: - Save
+  
+  private func saveItem(_ record: CKRecord) {
+    CKContainer
+      .default()
+      .publicCloudDatabase
+      .save(record) { [weak self] record, error in
+        guard let self else { return }
+        DispatchQueue.main.async {
+          self.textValue = ""
+        }
+      }
+  }
+  
+  // MARK: - Get
+  
+  func getItems() {
+    let predicate = NSPredicate(value: true)
+    let query = CKQuery(recordType: "Records", predicate: predicate)
+    query.sortDescriptors = [.init(key: "name", ascending: true)]
+    
+    let queryOperation = CKQueryOperation(query: query)
+//    queryOperation.resultsLimit = 25 // maxLimit: 100
+    
+    var records = [Item]()
+    
+    queryOperation.recordMatchedBlock = { id, result in
+      switch result {
+      case .success(let record):
+        guard let name = record["name"] as? String else {
+          return
+        }
+        records.append(.init(name: name, record: record))
+      case .failure(let error):
+        print("Reason: \(error)")
+      }
+    }
+    
+    queryOperation.queryResultBlock = { [weak self] result in
+      guard let self else { return }
+      switch result {
+      case .success(let value):
+        debugPrint("Query Result: \(value.debugDescription)")
+        
+        DispatchQueue.main.async {
+          self.records = records
+          self.getItems()
+        }
+        
+      case .failure(let error):
+        print("Reason: \(error)")
+      }
+    }
+    
+    addOperations(queryOperation)
+  }
+  
+  func addOperations(_ operation: CKDatabaseOperation) {
+    CKContainer
+      .default()
+      .publicCloudDatabase
+      .add(operation)
+  }
+  
+  // MARK: - Update
+  
+  func updateItem(_ item: Item) {
+    let record = item.record
+    record["name"] = "New Value"
+    
+    saveItem(record)
+    // Better fetch single Item, after updated
+  }
+  
+  // MARK: - Delete
+  
+  func deleteItem(_ indexSet: IndexSet) {
+    guard let index = indexSet.first else { return }
+    let item = records[index]
+    let record = item.record
+    
+    CKContainer
+      .default()
+      .publicCloudDatabase
+      .delete(withRecordID: record.recordID) { [weak self] id, error in
+        guard let self else { return }
+        DispatchQueue.main.async {
+          self.records.remove(at: index)
+        }
+      }
+  }
+}
+
+extension CloudKitViewModel {
+  
+  struct Item: Hashable {
+    let name: String
+    let record: CKRecord
   }
 }
--- CloudKitExample/ContentView.swift
+++ CloudKitExample/ContentView.swift
@@ -9,10 +9,82 @@ import SwiftUI
 
 struct ContentView: View {
   
-  @StateObject private var viewModel = CloudKitViewModel()
+  var body: some View {
+    TabView {
+      icloudCRUDview
+      icloudStatusView
+    }
+    .tabViewStyle(.page(indexDisplayMode: .never))
+  }
+  
+  // MARK: - CRUD
+  
+  var icloudCRUDview: some View {
+    NavigationStack {
+      VStack {
+        headerCrudView
+        listView
+      }
+      .safeAreaInset(edge: .bottom, content: inputViews)
+      .padding(.horizontal)
+    }
+  }
+  
+  private var headerCrudView: some View {
+    Text("CloudKit")
+      .font(.headline)
+  }
+  
+  private func inputViews() -> some View {
+    VStack {
+      textFieldView
+      addButton
+    }
+  }
+
+  private var textFieldView: some View {
+    TextField("Enter text", text: $viewModel.textValue)
+      .frame(height: 55)
+      .padding(.leading)
+      .background(Color.gray.opacity(0.2), in: RoundedRectangle(cornerRadius: 8))
+      .background(RoundedRectangle(cornerRadius: 8).stroke(lineWidth: 1))
+  }
+
+  private var addButton: some View {
+    Button {
+      viewModel.addButtonClicked()
+    } label: {
+      Text("Add")
+        .font(.headline)
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color.blue.opacity(0.1), in: RoundedRectangle(cornerRadius: 8))
+        .background(RoundedRectangle(cornerRadius: 8).stroke(lineWidth: 1))
+    }
+  }
+  
+  private var listView: some View {
+    List {
+      ForEach(viewModel.records, id: \.self) { record in
+        Text(record.name)
+          .onTapGesture {
+            viewModel.updateItem(record)
+          }
+      }
+      .onDelete(perform: viewModel.deleteItem)
+    }
+    .listStyle(.insetGrouped)
+    .frame(alignment: .leading)
+    .clipShape(RoundedRectangle(cornerRadius: 25.0))
+  }
+
 
   
-  var body: some View {
+  // MARK: - Status
+  
+  @StateObject private var viewModel = CloudKitViewModel()
+
+  var icloudStatusView: some View {
     VStack(alignment: .leading, spacing: 10) {
       Text("isSignedIn: \(viewModel.isSignedIn.description.capitalized)")
       Text("Error: \(viewModel.error.capitalized)")
